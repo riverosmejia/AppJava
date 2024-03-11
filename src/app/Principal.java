3a728a0c0a5cd64601cffb88755c499d339944d4
miguel angel riveros mejia <riverosmejiamiguel@gmail.com>
2024-03-11 11:07:37 -0500

    kilometraje inicial

--- src/app/Principal.java
+++ src/app/Principal.java
@@ -1,31 +1,27 @@
 package app;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import javax.swing.JFrame;
 import javax.swing.JTable;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Principal {
 
-    private Connection con=null;
+    private Connection con = null;
 
     public Principal() {
-        
+
         // Cargar el controlador JDBC de MariaDB
         try {
-        
             Class.forName("org.mariadb.jdbc.Driver");
-        
         } catch (ClassNotFoundException e) {
-        
             System.err.println("Error al cargar el controlador JDBC: " + e.getMessage());
-        
         }
 
         // Establecer la conexión en la clase Principal
-        Conexion Con = new Conexion("jdbc:mariadb://localhost:3306/qwerty","root","123");
-        this.con=Con.getConexion();
+        Conexion Con = new Conexion("jdbc:mariadb://localhost:3306/qwerty", "root", "123");
+        this.con = Con.getConexion();
         System.out.println(this.con);
     }
 
@@ -46,9 +42,33 @@ public class Principal {
     }
 
     public void Programa() {
+
+        FormularioAgregarCarro panelCarroA = new FormularioAgregarCarro(this.con);
         Panel1 panel = new Panel1(this.con);
-        Conexion Con = new Conexion("jdbc:mariadb://localhost:3306/qwerty","root","123");
-        
+
+        //panel.MostrarTable("Conductores");
+
+        // Obtén el componente JTable directamente de Panel1
+        JTable visor = panel.getVisor();
+        JFrame frame1 = panel.getFrame();
+
+        // Crea un JFrame y agrega el Panel1
+        JFrame frame = new JFrame("Vista de Conductores");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Agrega el Panel1 directamente al contenido del JFrame
+        frame.getContentPane().add(panelCarroA, java.awt.BorderLayout.PAGE_START);
+
+        // Configura el frame para ajustar el tamaño según el contenido
+        frame.pack();
+
+        // Configura el frame y hazlo visible
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void mostrarPanel1() {
+        Panel1 panel = new Panel1(this.con);
         panel.MostrarTable("Conductores");
 
         // Obtén el componente JTable directamente de Panel1
@@ -64,14 +84,15 @@ public class Principal {
         // Agrega el Panel1 directamente al contenido del JFrame
         frame.getContentPane().add(panel, java.awt.BorderLayout.PAGE_END);
 
+        // Configura el frame para ajustar el tamaño según el contenido
+        frame.pack();
+
         // Configura el frame y hazlo visible
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Muestra en pantalla completa
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Crea una instancia de Principal y ejecuta el programa
         Principal principal = new Principal();
         principal.Programa();
     }
